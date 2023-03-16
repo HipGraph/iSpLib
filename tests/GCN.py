b68c58dbe67080539be776e1cc23e292a7cffe6e
Onix Hoque <onix.hooque@gmail.com>
2023-03-16 00:31:05 -0400

    Test files updated for GCN

--- tests/GCN.py
+++ tests/GCN.py
@@ -1,5 +1,5 @@
 import builtins
-builtins.TRAINING_STATUS = True
+builtins.FUSEDMM = True
 
 
 import torch
@@ -39,7 +39,7 @@ model = Net().to(device)
 data = dataset[0].to(device)
 
 def train_GCN():
-  builtins.TRAINING_STATUS = True
+  builtins.FUSEDMM = True
   optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
   model.train()
@@ -56,7 +56,7 @@ def train_GCN():
       print('Epoch: %d, Accuracy: %.4f'%(epoch,acc))
 
 def test_GCN(status):
-  builtins.TRAINING_STATUS = status  # Use FusedMM for inference
+  builtins.FUSEDMM = status  # Use FusedMM or not
   _, pred = model(data).max(dim=1)
   correct = float (pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
   acc = correct / data.test_mask.sum().item()
@@ -80,17 +80,18 @@ print("Training GCN...")
 train_GCN()
 print("Done!")
 
-print("Accuracy without FusedMM: ", test_GCN(True))
-print("Accuracy with FusedMM: ", test_GCN(False))
+print("Accuracy without FusedMM: ", test_GCN(False))
+print("Accuracy with FusedMM: ", test_GCN(True))
 
 import io
 
 def get_cumulative_time(FusedMM=False):
     with cProfile.Profile() as pr:
-        test_GCN(not FusedMM)
+        test_GCN(FusedMM)
         txt = io.StringIO()
         p = pstats.Stats(pr, stream=txt)
         p.print_stats('torch_sparse.spmm_sum' if not FusedMM else 'torch_sparse.fusedmm_spmm')
+        # print(txt.getvalue())
         return txt.getvalue().strip().split('\n')[-1].split(' ')[-4]
     
 torch_op_time = float(get_cumulative_time(False))
