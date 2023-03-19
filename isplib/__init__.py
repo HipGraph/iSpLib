98080f7c0f06ddfecbd46d063a5b9972f67daddc
G Rohit <gampa@Rohit.local>
2023-03-19 14:08:02 -0400

    reduction attempt 1

--- isplib/__init__.py
+++ isplib/__init__.py
@@ -6,8 +6,7 @@ import torch
 __version__ = '0.6.16'
 
 for library in [
-        '_version', '_convert', '_diag', '_spmm', '_metis', '_rw', '_saint',
-        '_sample', '_ego_sample', '_hgt_sample', '_neighbor_sample', '_relabel', '_fusedmm'
+        '_version', '_spmm', '_fusedmm'
 ]:
     cuda_spec = importlib.machinery.PathFinder().find_spec(
         f'{library}_cuda', [osp.dirname(__file__)])
@@ -38,76 +37,13 @@ if torch.version.cuda is not None and cuda_version != -1:  # pragma: no cover
 
 from .storage import SparseStorage  # noqa
 from .tensor import SparseTensor  # noqa
-from .transpose import t  # noqa
-from .narrow import narrow, __narrow_diag__  # noqa
-from .select import select  # noqa
-from .index_select import index_select, index_select_nnz  # noqa
-from .masked_select import masked_select, masked_select_nnz  # noqa
-from .permute import permute  # noqa
-from .diag import remove_diag, set_diag, fill_diag, get_diag  # noqa
-from .add import add, add_, add_nnz, add_nnz_  # noqa
-from .mul import mul, mul_, mul_nnz, mul_nnz_  # noqa
-from .reduce import sum, mean, min, max  # noqa
-from .matmul import matmul  # noqa
-from .cat import cat  # noqa
-from .rw import random_walk  # noqa
-from .metis import partition  # noqa
-from .bandwidth import reverse_cuthill_mckee  # noqa
-from .saint import saint_subgraph  # noqa
-from .sample import sample, sample_adj  # noqa
 
-from .convert import to_isplib, from_isplib  # noqa
-from .convert import to_scipy, from_scipy  # noqa
-from .coalesce import coalesce  # noqa
-from .transpose import transpose  # noqa
-from .eye import eye  # noqa
-from .spmm import spmm  # noqa
-from .spspmm import spspmm  # noqa
-from .spadd import spadd  # noqa
+from .matmul import matmul  # noqa
+
 
 __all__ = [
     'SparseStorage',
     'SparseTensor',
-    't',
-    'narrow',
-    '__narrow_diag__',
-    'select',
-    'index_select',
-    'index_select_nnz',
-    'masked_select',
-    'masked_select_nnz',
-    'permute',
-    'remove_diag',
-    'set_diag',
-    'fill_diag',
-    'get_diag',
-    'add',
-    'add_',
-    'add_nnz',
-    'add_nnz_',
-    'mul',
-    'mul_',
-    'mul_nnz',
-    'mul_nnz_',
-    'sum',
-    'mean',
-    'min',
-    'max',
     'matmul',
-    'cat',
-    'random_walk',
-    'partition',
-    'reverse_cuthill_mckee',
-    'saint_subgraph',
-    'to_isplib',
-    'from_isplib',
-    'to_scipy',
-    'from_scipy',
-    'coalesce',
-    'transpose',
-    'eye',
-    'spmm',
-    'spspmm',
-    'spadd',
     '__version__',
 ]
