dc8ebe7049b5d4732cc9ad9546ae5bd7af6d9262
Onix Hoque <onix.hooque@gmail.com>
2024-08-15 00:13:06 -0400

    GPU code removed from setup and configure. Cache system for mutliple SpMM added alhumdulillah. Might contain bugs

--- csrc/fusedmm.cpp
+++ csrc/fusedmm.cpp
@@ -86,28 +86,28 @@ int fusedMM_csr
 
 }
 
-void fusedmm_cuda
-(
-   const char tkern,       // kernel variations
-   const INDEXTYPE m,      // rows of A 
-   const INDEXTYPE n,      // rows of B
-   const INDEXTYPE k,      // dimension: col of A and B
-   const VALUETYPE alpha,  // not used yet  
-   const INDEXTYPE nnz,    // nonzeros  
-   const INDEXTYPE rows,   // number of rows for sparse matrix 
-   const INDEXTYPE cols,   // number of columns for sparse matrix 
-   const VALUETYPE *val,   // NNZ value  
-   const INDEXTYPE *indx,  // colids -> column indices 
-   const INDEXTYPE *pntrb, // starting index for rowptr
-   const INDEXTYPE *pntre, // ending index for rowptr
-   const VALUETYPE *a,     // Dense A (X) matrix
-   const INDEXTYPE lda,    // leading dimension of A (col size since A row-major)  
-   const VALUETYPE *b,     // Dense B matrix
-   const INDEXTYPE ldb,    // leading dimension of B (col size since B row-major)  
-   const VALUETYPE beta,   // beta value 
-   VALUETYPE *c,           // Dense matrix c
-   const INDEXTYPE ldc     // leading dimension of c (col size since C row-major) 
-);
+// void fusedmm_cuda
+// (
+//    const char tkern,       // kernel variations
+//    const INDEXTYPE m,      // rows of A 
+//    const INDEXTYPE n,      // rows of B
+//    const INDEXTYPE k,      // dimension: col of A and B
+//    const VALUETYPE alpha,  // not used yet  
+//    const INDEXTYPE nnz,    // nonzeros  
+//    const INDEXTYPE rows,   // number of rows for sparse matrix 
+//    const INDEXTYPE cols,   // number of columns for sparse matrix 
+//    const VALUETYPE *val,   // NNZ value  
+//    const INDEXTYPE *indx,  // colids -> column indices 
+//    const INDEXTYPE *pntrb, // starting index for rowptr
+//    const INDEXTYPE *pntre, // ending index for rowptr
+//    const VALUETYPE *a,     // Dense A (X) matrix
+//    const INDEXTYPE lda,    // leading dimension of A (col size since A row-major)  
+//    const VALUETYPE *b,     // Dense B matrix
+//    const INDEXTYPE ldb,    // leading dimension of B (col size since B row-major)  
+//    const VALUETYPE beta,   // beta value 
+//    VALUETYPE *c,           // Dense matrix c
+//    const INDEXTYPE ldc     // leading dimension of c (col size since C row-major) 
+// );
 
 
 std::tuple<torch::Tensor, torch::optional<torch::Tensor>> fusedmm_spmm_fw(torch::Tensor rowptr, torch::Tensor col, torch::optional<torch::Tensor> value, torch::Tensor mat, int reduction = 0)
@@ -190,9 +190,9 @@ std::tuple<torch::Tensor, torch::optional<torch::Tensor>> fusedmm_spmm_fw(torch:
 	  // start = std::chrono::system_clock::now();
     if (rowptr.device().is_cuda())
     {
-      // std::cout << "Invoking FusedMM CUDA\n" ;
+      std::cout << "Invoking FusedMM CUDA\n" ;
       // const char tkern = 'm';
-      fusedmm_cuda(tkern, M, N, K, alpha, S_nnz, S_rows, S_cols, S_values, S_colids, S_rowptr, S_rowptr + 1, a, lda, b, ldb, beta, c, ldc);
+      // fusedmm_cuda(tkern, M, N, K, alpha, S_nnz, S_rows, S_cols, S_values, S_colids, S_rowptr, S_rowptr + 1, a, lda, b, ldb, beta, c, ldc);
     }
     else{
       fusedMM_csr(imsg, M, N, K, alpha, S_nnz, S_rows, S_cols, S_values, S_colids, S_rowptr, S_rowptr + 1, a, lda, b, ldb, beta, c, ldc, c_idx);
--- isplib/__init__.py
+++ isplib/__init__.py
@@ -33,7 +33,9 @@ for library in ['_fusedmm']:
 
 class iSpLibPlugin:
 	backup = []    
+	value_cache = {}
 	cache = {}
+	row_cache = {}
 	is_cached = False
 	value_cached = False
 
@@ -45,12 +47,14 @@ class iSpLibPlugin:
 		# try:
 		def spmm_autotuned(src, other, reduce: str = "sum") -> torch.Tensor:
 			rowptr, col, value = src.csr()
+			cache_idx = (rowptr.data_ptr(), col.data_ptr())
 			if value is None:
-				if iSpLibPlugin.value_cached == False:
+				# if iSpLibPlugin.value_cached == False:
+				if cache_idx not in iSpLibPlugin.value_cache:
 					value = torch.ones_like(col, dtype=torch.float32)
-					iSpLibPlugin.cache['value'] = value
+					iSpLibPlugin.value_cache[cache_idx] = value
 				else:
-					value = iSpLibPlugin.cache['value'] = value
+					value = iSpLibPlugin.value_cache[cache_idx]
 			row = src.storage._row
 			rowcount = src.storage._rowcount
 			csr2csc = src.storage._csr2csc
@@ -68,13 +72,17 @@ class iSpLibPlugin:
 				csr2csc = src.storage.csr2csc()
 				colptr = src.storage.colptr()
 			
-			if iSpLibPlugin.is_cached == False and reduce=='sum':
-				iSpLibPlugin.is_cached = True
-				iSpLibPlugin.cache['index_select'] = value.view([-1, 1]).index_select(0, csr2csc).view(-1)
-				iSpLibPlugin.cache['row_select'] = row.index_select(0, csr2csc)
+			# if iSpLibPlugin.is_cached == False and reduce=='sum':
+			if cache_idx not in iSpLibPlugin.cache and reduce=='sum':
+				# iSpLibPlugin.is_cached = True
+				iSpLibPlugin.cache[cache_idx] = {}
+				iSpLibPlugin.cache[cache_idx]['index_select'] = value.view([-1, 1]).index_select(0, csr2csc).view(-1)
+				iSpLibPlugin.cache[cache_idx]['row_select'] = row.index_select(0, csr2csc)
 			
-			if iSpLibPlugin.is_cached == False and reduce=='mean' and other.requires_grad:
-				iSpLibPlugin.is_cached = True
+			# if iSpLibPlugin.is_cached == False and reduce=='mean' and other.requires_grad:
+			if cache_idx not in iSpLibPlugin.cache and reduce=='mean' and other.requires_grad:
+				# iSpLibPlugin.is_cached = True
+				iSpLibPlugin.cache[cache_idx] = {}
 				new_row = row.index_select(0, csr2csc)
 				new_rowcount = rowcount.index_select(0, row).type(other.type())
 				new_rowcount.masked_fill_(new_rowcount < 1, 1)
@@ -83,15 +91,19 @@ class iSpLibPlugin:
 					new_rowcount = value.view([-1, 1]).index_select(0, csr2csc).view(-1).div(new_rowcount)
 				else:
 					new_rowcount.pow_(-1)
-				print(new_row.dtype, new_rowcount.dtype)
-
-				iSpLibPlugin.cache['new_row'] = new_row
-				iSpLibPlugin.cache['new_rowcount'] = new_rowcount
+				# print(new_row.dtype, new_rowcount.dtype)
+				
+				if cache_idx not in iSpLibPlugin.row_cache:
+					iSpLibPlugin.row_cache[cache_idx] = {}
+					iSpLibPlugin.row_cache[cache_idx]['new_row'] = new_row
+					iSpLibPlugin.row_cache[cache_idx]['new_rowcount'] = new_rowcount
 				
 			
-			if 'new_row' not in iSpLibPlugin.cache:
-				iSpLibPlugin.cache['new_row'] = torch.ones([1])
-				iSpLibPlugin.cache['new_rowcount'] = torch.ones([1])
+			# if 'new_row' not in iSpLibPlugin.cache:
+			if cache_idx not in iSpLibPlugin.row_cache:
+				iSpLibPlugin.row_cache[cache_idx] = {}
+				iSpLibPlugin.row_cache[cache_idx]['new_row'] = torch.ones([1])
+				iSpLibPlugin.row_cache[cache_idx]['new_rowcount'] = torch.ones([1])
 			# print()
 			# print()
 			# print('---')
@@ -126,7 +138,7 @@ class iSpLibPlugin:
 			# print(b)
 			# print('---')
 			if reduce in ['sum', 'add']:
-				out = torch.ops.isplib.fusedmm_spmm(row, rowptr, col, value, colptr, csr2csc, other, iSpLibPlugin.cache['index_select'], iSpLibPlugin.cache['row_select'])
+				out = torch.ops.isplib.fusedmm_spmm(row, rowptr, col, value, colptr, csr2csc, other, iSpLibPlugin.cache[cache_idx]['index_select'], iSpLibPlugin.cache[cache_idx]['row_select'])
 			elif reduce == 'max':
 				out = torch.ops.isplib.fusedmm_spmm_max(rowptr, col, value, other)
 			elif reduce == 'min':
@@ -136,7 +148,7 @@ class iSpLibPlugin:
 				
 			#    print('Torchsparse output: ')
 			#    print(torch.ops.torch_sparse.spmm_mean(row, rowptr, col, value, rowcount, colptr, csr2csc, other))
-				out = torch.ops.isplib.fusedmm_spmm_mean(row, rowptr, col, value, rowcount, colptr, csr2csc, other, iSpLibPlugin.cache['new_row'], iSpLibPlugin.cache['new_rowcount'])
+				out = torch.ops.isplib.fusedmm_spmm_mean(row, rowptr, col, value, rowcount, colptr, csr2csc, other, iSpLibPlugin.row_cache[cache_idx]['new_row'], iSpLibPlugin.row_cache[cache_idx]['new_rowcount'])
 			#    out = torch.ops.torch_sparse.spmm_mean(row, rowptr, col, value, rowcount, colptr, csr2csc, other)
 				
 			else:
